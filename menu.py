aace63f4bc6d48060a49d59b3a6eca2b98a6f69d
bielrz174 <gabrielperimgomes@gmail.com>
2026-03-19 18:37:14 -0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -13,30 +13,36 @@ def executar_programa(nome_arquivo):
     except Exception as e:
         messagebox.showerror("Erro", f"Não foi possível abrir o programa: {e}")
 
-# Configuração da Janela Principal
+def funcaoFechar():
+    janela.quit()
+
+def funcaoExecutar():
+    executar_programa("login_menu.py")
+
+def funcaoWrapper():
+    funcaoExecutar()
+    funcaoFechar()
+
 janela = tk.Tk()
 janela.title("Meu Menu de Programas")
 janela.geometry("300x250")
 
-# Título visual
 label_titulo = tk.Label(janela, text="Selecione um Programa", font=("Arial", 14, "bold"))
 label_titulo.pack(pady=20)
 
-# Botões para chamar os scripts
+
 btn1 = tk.Button(janela, text="Abrir Conversor", width=20,
                  command=lambda: executar_programa("temperatura.py"))
 btn1.pack(pady=5)
-
 
 btn2 = tk.Button(janela, text="Abrir Calculadora de IMC", width=20,
                  command=lambda: executar_programa("calculadora_imc.py"))
 btn2.pack(pady=5)
 
 btn3 = tk.Button(janela, text="Login", width=20,
-                 command=lambda: executar_programa("login_menu.py"))
+                 command=lambda: funcaoWrapper())
 btn3.pack(pady=5)
 
-# Botão de Sair
 btn_sair = tk.Button(janela, text="Sair", width=20, fg="red", command=janela.quit)
 btn_sair.pack(pady=20)
 
